1510b39747da44fa0d3f7a5c3da92857fbbf5258
Georgi Komov <gogo777@mail.bg>
2025-09-19 00:40:37 +0300

    List Comprehension update

--- find_the_largest.py
+++ find_the_largest.py
@@ -1,4 +1,8 @@
-numbers = sorted(input(), reverse = True)
+#numbers = sorted(input(), reverse = True)
 
-for number in numbers:
-    print(number, end="")
+#numbers = [print(int(x), end="") for x in sorted(input(), reverse = True)]
+number_2 = print(int("".join(sorted(input(), reverse=True))))
+
+
+#for number in numbers:
+   # print(number, end="")#
