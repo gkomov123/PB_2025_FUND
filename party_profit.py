f808e0f7e4cc3a4e7846a87998fbb434238f5800
Georgi Komov <69540742+gkomov123@users.noreply.github.com>
2025-09-24 02:52:19 +0300

    Update party_profit.py
    fixed bug

--- party_profit.py
+++ party_profit.py
@@ -2,7 +2,7 @@ group_size = int(input())
 days = int(input())
 coins = 0
 
-for day in range(1, days, + 1):
+for day in range(1, days + 1):
     if day % 10 == 0:
         group_size -= 2
     if day % 15 == 0:
