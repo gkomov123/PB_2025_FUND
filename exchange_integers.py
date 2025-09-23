1de502d4733963ab9db000b86ca3f65f8136d08e
Georgi Komov <69540742+gkomov123@users.noreply.github.com>
2025-09-24 00:49:21 +0300

    number exchange problem
    solved

---
+++ exchange_integers.py
@@ -0,0 +1,12 @@
+number1 = int(input())
+number2 = int(input())
+
+number1_exchange = number2
+number2_exchange = number1
+
+print("Before:")
+print(f"a = {number1}")
+print(f"b = {number2}")
+print("After:")
+print(f"a = {number1_exchange}")
+print(f"b = {number2_exchange}")
