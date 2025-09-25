6ad3f6ff5fa504a5979374f0405610c3303ba2ad
Georgi Komov <69540742+gkomov123@users.noreply.github.com>
2025-09-25 23:11:34 +0300

    comments + solved

--- survival_of_the_biggest.py
+++ survival_of_the_biggest.py
@@ -1,3 +1,5 @@
+# map() and min() not used to practice basic loops and list looping
+
 numbers_as_string = input().split()
 numbers_to_remove = int(input())
 numbers_as_integer = []
@@ -6,15 +8,15 @@ for number in numbers_as_string: #Iterate and convert from string to integer
     numbers_as_integer.append(int(number))
 
 for _ in range(numbers_to_remove): # Removal interations
-    #smallest = min(numbers_as_integer)
+    #smallest = min(numbers_as_integer) can be used to simplify
     smallest = numbers_as_integer[0]
     for num in numbers_as_integer: # Iterate thru the numbers list
-        if num < smallest: # check if the current number  is smaller than variable smallest->0
+        if num < smallest:
             smallest = num # if smaller become the smallest number
     numbers_as_integer.remove(smallest) # remove the smallest number
 
 survival_numbers = []
-for number in numbers_as_integer:
+for number in numbers_as_integer: #convert back to string list to use .join
     survival_numbers.append(str(number))
 
-print(", ".join(survival_numbers))
+print(", ".join(survival_numbers)) #print the survived numbers
