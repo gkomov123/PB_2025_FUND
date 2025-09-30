b1d3d0b980e4cd8a1303a68262e1f12b2b4270b7
Georgi Komov <69540742+gkomov123@users.noreply.github.com>
2025-09-30 21:45:42 +0300

    orders problem solved

---
+++ oreders.py
@@ -0,0 +1,15 @@
+product_input = input()
+quantity_input = int(input())
+
+def price_calculator(product:str, quantity:int) -> float:
+    if product == "coffee":
+        return quantity * 1.50
+    elif product == "water":
+        return quantity * 1.00
+    elif product == "coke":
+        return quantity * 1.40
+    elif product == "snacks":
+        return quantity * 2.00
+    
+result = price_calculator(product_input, quantity_input)
+print(f"{result:.2f}")
