b9af468675ae0f35f01058178d8bb25e1d6d6eaf
Georgi Komov <69540742+gkomov123@users.noreply.github.com>
2025-10-04 03:45:50 +0300

    switched from a list to a integer

--- perfect_number.py
+++ perfect_number.py
@@ -1,9 +1,9 @@
 def is_perfect_number(number):
-    divisors = []
+    divisors_sum = 0
     for divisor in range(1, number):
         if number % divisor == 0:
-            divisors.append(divisor)
-    if sum(divisors) == number:
+            divisors_sum += divisor
+    if divisors_sum == number:
         return "We have a perfect number!"
     return "It's not so perfect."
 
