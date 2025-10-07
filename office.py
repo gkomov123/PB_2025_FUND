9ebacc4a140af62d53706b0d1951abf87a14dc83
Georgi Komov <69540742+gkomov123@users.noreply.github.com>
2025-10-07 23:19:46 +0300

    renamed variables and optimized

--- office.py
+++ office.py
@@ -1,13 +1,16 @@
-employees = input().split()
+employees_happines = input().split()
 improvement_factor = int(input())
 
 
-employees_ = [int(x*improvement_factor) for x in employees]
+improved_happines = [int(x) * improvement_factor for x in employees_happines]
 
 #avarage_happines = [x for x in multiplied_employee if sum(multiplied_employee) / len(multiplied_employee)]
-filtered = list(filter(lambda x: x >= (sum(employees_) / len(employees_)), employees_))
+# filtered = list(filter(lambda x: x >= (sum(employees_) / len(employees_)), employees_))
 
-if len(filtered) >= len(employees_) / 2:
-    print(f"Score: {len(filtered)}/{len(employees_)}. Employees are happy!")
+avarage = sum(improved_happines) / len(improved_happines)
+filtered = [x for x in improved_happines if x >= avarage]
+
+if len(filtered) >= len(improved_happines) / 2:
+    print(f"Score: {len(filtered)}/{len(improved_happines)}. Employees are happy!")
 else:
-    print(f"Score: {len(filtered)}/{len(employees_)}. Employees are not happy!")
+    print(f"Score: {len(filtered)}/{len(improved_happines)}. Employees are not happy!")
