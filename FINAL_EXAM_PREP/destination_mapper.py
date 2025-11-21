aeaed6abb73307052acf00166ef386c856def5b3
Georgi Komov <69540742+gkomov123@users.noreply.github.com>
2025-11-21 04:30:51 +0200

    optimize

--- FINAL_EXAM_PREP/destination_mapper.py
+++ FINAL_EXAM_PREP/destination_mapper.py
@@ -1,18 +1,12 @@
 import re
 
-regex = r'([=][A-Z]{1}[A-Za-z]{2,}[=])|([\/][A-Z]{1}[A-Za-z]{2,}[\/])'
+regex = r'([=/])([A-Z][A-Za-z]{2,})\1'
 string_to_manipulate = input()
 
 matches = re.findall(regex, string_to_manipulate)
-total_points = 0
-final_matches = []
-
-for cities_tuple in matches:
-    for city in cities_tuple:
-        if city == '':
-            continue
-        final_matches.append(city[1:len(city) - 1])
-        total_points += len(city[1:len(city) - 1])
+print(matches)
+final_matches = [match[1] for match in matches]
+total_points = sum(len(city) for city in final_matches)
 
 print('Destinations: ' + ', '.join(final_matches))
 print(f'Travel Points: {total_points}')
